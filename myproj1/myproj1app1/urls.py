e7b87375b7f0f798f28f150e30c8cf9546a0b7ea
Lavanya V <122570170+Lavanya1034@users.noreply.github.com>
2024-04-23 01:08:03 +0400

    Update urls.py

--- myproj1/myproj1app1/urls.py
+++ myproj1/myproj1app1/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Notes
+from .views import NoteList,NoteDetail
 
-urlpatterns = [
-    path('',Notes.as_view(),name='note-list')
+urlpatterns=[
+    path("",NoteList.as_view(),name="note-list"),
+    path("<str:pk>",NoteDetail.as_view(),name="note-detail")
 ]
